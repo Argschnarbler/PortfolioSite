533314d708f52435c6cdf128a06a9f0cf4ebe93e
Argschnarbler <jacobmeikle7@gmail.com>
2014-01-28 15:19:44 -0500

    Done

--- js/linkScroller.js
+++ js/linkScroller.js
@@ -1,3 +1,4 @@
+//<!-- File: linkScroller.js Author: Jacob Meikle Website: Assignment1 File Desc: Makes the navigate smoothly scroll between sections. -->
 $('a').click(function(){
 		    $('html, body').animate({
 		        scrollTop: $( $.attr(this, 'href') ).offset().top
